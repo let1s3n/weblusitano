02b6f31f44b4baa47aea6fddbbc58fdc8ad4e05a
let1s3n <letisendev@gmail.com>
2023-04-28 07:41:43 -0500

    fix

--- src/components/templates/Home/Home.jsx
+++ src/components/templates/Home/Home.jsx
@@ -4,12 +4,12 @@ import { BsLinkedin } from "react-icons/bs";
 const Home = () => {
 
   return (
-    <Container className="g-0 d-flex flex-column align-items-center justify-content-center" style={{ height: "100vh", rowGap: "2rem" }}>
-      <Row className="g-0 justify-content-center">
+    <Container fluid className="mainContainer g-0 d-flex flex-column align-items-center justify-content-center fw-bolder" style={{ height: "100vh", rowGap: "2rem" }}>
+      {/* <Row className="g-0 justify-content-center">
         <Col xs="auto" className="g-0">
           <Image className="logo" src="/images/lusitanoBanner.jpeg" />
         </Col>
-      </Row>
+      </Row> */}
 
       <Row className="g-0 justify-content-center">
         <Col xs="auto" className="g-0 text-center">
@@ -18,12 +18,13 @@ const Home = () => {
             href="https://www.linkedin.com/company/lusitano-maritime-inc/"
             target='_blank'
           >
-            <h3 className="lh-1 m-0 p-0">
+            {/* <h3 className="lh-1 m-0 p-0 text-secondary">
               <BsLinkedin />
-            </h3>
+            </h3> */}
+            <Image src="/images/linkedin.png" />
           </a>
 
-          <p className="lh-1 m-0">info@lusitanomaritime.com</p>
+          <p className="lh-1 m-0 mt-3 text-primary">info@lusitanomaritime.com</p>
         </Col>
       </Row>
 
@@ -32,7 +33,7 @@ const Home = () => {
         style={{ rowGap: 2 + "rem" }}
       >
         <Col xl={4}>
-          <p className="m-0 text-center">
+          <p className="m-0 text-center text-primary">
             BICSA Financial Center Avenida Balboa y Calle Aquilino De La Guardia Oficina 4703 Piso 47 PH Ciudad de Panamá
             0801, Ciudad de Panamá, Panama 0801, PA
           </p>
